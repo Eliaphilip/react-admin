6aa217a199268cc774fc9f60e7036a2f5670023b
Elia_Philip <130383764+Eliaphilip@users.noreply.github.com>
2025-05-14 00:49:13 +0300

    top bar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -14,15 +14,25 @@ const Topbar = () => {
     const colours = tokens (theme.palette.mode);
     const colourMode = useContext(ColourModeContext);
 
-    return (<Box display = "flex" justifyContent="space-between" p= {2} >
+    return (
+    <Box display = "flex" justifyContent="space-between" p= {2} >
         {/* SEARCH BAR */}
         <Box 
         display="flex" 
         backgroundColour = {colours.primary[400]}
-        borderRadius = "3px">
+        borderRadius = "3px"
+        >
 
+        <InputBase sx = {{ml: 2, flex:1}} placeholder="Search" />
+        <IconButton type = "button" sx = {{p: 1}}>
+        <SearchIcon/>   
+        </IconButton>
         </Box>
+        
+        {/* ICONS */}
         <IconButton></IconButton>
-    </Box>)
-}
+        </Box>
+         );
+        
+};
 export default Topbar;
